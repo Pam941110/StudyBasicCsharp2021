015192792931f334df97b4b7c132fb5a751c548a
Pam941110 <cronkic1234@gmail.com>
2021-05-03 10:34:03 +0900

    First Commit

---
+++ TotalSolution/ConsoleFristApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleFristApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
